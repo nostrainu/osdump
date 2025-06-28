afb44357b7fb6017d550365461059fb6e5a01069
nostra <141655452+nostrainu@users.noreply.github.com>
2025-06-28 13:03:07 +0800

    Update uzuloadstring.lua

--- Folder/uzuloadstring.lua
+++ Folder/uzuloadstring.lua
@@ -1,20 +1,19 @@
-shared.weight = 100
-getgenv().key = "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"
+getgenv().key = "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"
 
 getgenv().get_github_file = function(file)
     local token = key
 
     local user = "uzu01"
     local repo = "private"
-    local url = ("https://api.github.com/repos/%*/%*/contents/%*"):format(user, repo, file)
+    local url = ("https://api.github.com/repos/%*/%*/contents/%*%22):format(user, repo, file)
 
-    local auth = crypt.base64.encode(("%*:%*"):format(user, token))
+    local auth = crypt.base64.encode(("%:%"):format(user, token))
     local headers = {["Authorization"] = ("Basic %*"):format(auth)}
     local result = game:GetService("HttpService"):JSONDecode(request({Url = url, Method = "GET", Headers = headers}).Body)
 
     local link =  rawget(result, "download_url")
     local succ, res = pcall(function()
-        return game:HttpGet(link)    
+        return game:HttpGet(link)
     end)
 
     if not link then print("wrong key", file) return end
@@ -22,4 +21,4 @@ getgenv().get_github_file = function(file)
     return loadstring(res)()
 end
 
-get_github_file("init.lua")
+get_github_file("init.lua") 
