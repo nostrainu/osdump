f188cdfcbb80dffe6a53641fbc435cb310e825c3
nostra <141655452+nostrainu@users.noreply.github.com>
2025-07-03 17:21:34 +0800

    Create mooncat.lua

---
+++ Folder/mooncat.lua
@@ -0,0 +1,162 @@
+--// Library and Config
+local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
+local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
+local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
+local http = game:GetService("HttpService")
+local folder, path = "grangrant", "grant/config.json"
+
+local defaults = {
+    AutoIdle = false,
+}
+
+local function save()
+    if not isfolder(folder) then makefolder(folder) end
+    writefile(path, http:JSONEncode(config))
+end
+
+local config = isfile(path) and http:JSONDecode(readfile(path)) or {}
+
+for k, v in pairs(defaults) do
+    config[k] = config[k] == nil and v or config[k]
+    getgenv()[k] = config[k]
+end
+
+getgenv().config = config
+
+Library.ForceCheckbox = false
+Library.ShowToggleFrameInKeybinds = true
+
+local Window = Library:CreateWindow({
+    Title = "Grant",
+    Footer = "v0.01",
+    MobileButtonsSide = "Left",
+    NotifySide = "Right",
+    Center = true,
+    Size = Library.IsMobile and UDim2.fromOffset(450, 300) or UDim2.fromOffset(650, 500),
+    ShowCustomCursor = false,
+})
+
+
+--// Tabs
+local Tabs = {
+    Main = Window:AddTab("Main", "user"),
+    ["UI Settings"] = Window:AddTab("UI Settings", "settings"),
+}
+
+--// Main Functionalities
+local uiActive = true
+
+--// Menu
+local LeftGroupBox = Tabs.Main:AddLeftGroupbox("Idle")
+
+LeftGroupBox:AddToggle("MoonCat", {
+    Text = "Auto Idle",
+    Default = getgenv().AutoIdleToggle,
+    Callback = function(val)
+        getgenv().AutoIdleToggle = val
+        config.AutoIdleToggle = val
+        save()
+        if not val then
+            getgenv().AutoIdle = false  
+        end
+    end
+})
+
+--// Modules and Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local GetPetCooldown = ReplicatedStorage:WaitForChild("GameEvents"):WaitForChild("GetPetCooldown")
+local IdleHandler = require(ReplicatedStorage.Modules.PetServices.PetActionUserInterfaceService.PetActionsHandlers.Idle)
+
+--// Auto Idle
+task.spawn(function()
+    while uiActive do
+        if getgenv().AutoIdle then
+            for _, v in ipairs(workspace.PetsPhysical:GetChildren()) do
+                if v:IsA("BasePart") and v.Name == "PetMover" then
+                    local model = v:FindFirstChild(v:GetAttribute("UUID"))
+                    if model and model:IsA("Model") and model:GetAttribute("CurrentSkin") == "Moon Cat" then
+                        task.spawn(IdleHandler.Activate, v)
+                    end
+                end
+            end
+        end
+        task.wait()
+    end
+end)
+
+--// Echo Frog
+task.spawn(function()
+    while uiActive do
+        if getgenv().AutoIdleToggle then 
+            for _, v in ipairs(workspace.PetsPhysical:GetChildren()) do
+                if v:IsA("BasePart") and v.Name == "PetMover" then
+                    local uuid = v:GetAttribute("UUID")
+                    local model = uuid and v:FindFirstChild(uuid)
+
+                    if model and model:IsA("Model") and model:GetAttribute("CurrentSkin") == nil then
+                        local ok, cooldowns = pcall(GetPetCooldown.InvokeServer, GetPetCooldown, uuid)
+                        if ok and typeof(cooldowns) == "table" then
+                            for _, cd in ipairs(cooldowns) do
+                                local time = tonumber(cd.Time)
+                                if time and time <= 76 and time >= 73 and not getgenv().AutoIdle then
+                                    print("Idle = True")
+                                    getgenv().AutoIdle = true
+                                    task.delay(10, function()
+                                        getgenv().AutoIdle = false
+                                        print("Idle = False(10s)")
+                                    end)
+                                    break
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        else
+            getgenv().AutoIdle = false
+        end
+        task.wait(1)
+    end
+end)
+
+--// Menu
+local MenuGroup = Tabs["UI Settings"]:AddLeftGroupbox("Menu")
+
+MenuGroup:AddDropdown("NotificationSide", {
+    Values = { "Left", "Right" },
+    Default = "Right",
+    Text = "Notification Side",
+    Callback = function(Value)
+        Library:SetNotifySide(Value)
+    end,
+})
+
+MenuGroup:AddDropdown("DPIDropdown", {
+    Values = { "50%", "75%", "100%", "125%", "150%", "175%", "200%" },
+    Default = "100%",
+    Text = "DPI Scale",
+    Callback = function(Value)
+        Value = Value:gsub("%%", "")
+        local DPI = tonumber(Value)
+        Library:SetDPIScale(DPI)
+    end,
+})
+
+MenuGroup:AddDivider()
+MenuGroup:AddLabel("Menu bind")
+    :AddKeyPicker("MenuKeybind", { Default = "LeftControl", NoUI = true, Text = "Menu keybind" })
+
+MenuGroup:AddButton("Unload", function()
+    uiActive = false
+    Library:Unload()
+end)
+
+Library.ToggleKeybind = Library.Options.MenuKeybind
+
+ThemeManager:SetLibrary(Library)
+ThemeManager:SetFolder("hikochairs")
+ThemeManager:ApplyToTab(Tabs["UI Settings"])
+
+Library:OnUnload(function()
+    uiActive = false
+end)
